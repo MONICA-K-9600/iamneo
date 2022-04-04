0888e4bcbb544b4da4c703fffde6dcc869060e6e
HP <monicamonisa2@gmail.com>
2022-04-04 16:53:13 +0530

    Final changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { SearchPipe } from './search.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
--- src/app/models/column.model.ts
+++ src/app/models/column.model.ts
@@ -1,3 +1,1 @@
-export class Column {
-    constructor(public name: string, public tasks: string[]) {}
-}
+export class Column {    constructor(public name: string, public tasks: string[]) {}}
