e9e8eaf69ae136dba1e2220300bed7bf92f91a29
HP <monicamonisa2@gmail.com>
2022-04-05 09:21:18 +0530

    commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,6 @@
 const { appendFile } = require("fs")
-const port = process.env.port || 5000;
-app.listen(port)
+const app = fs();
+app.get('/', function(req, res) {
+    res.send("working");
+})
+app.listen(process.env.port || 5000);
